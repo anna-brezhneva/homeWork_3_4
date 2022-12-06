dc6394c0d3604a2f3f2d54ef0c20eedfd08afc23
anna brezhneva <brezhneva.a@yandex.ru>
2022-12-06 21:59:35 +0300

    Недорешала задачу №27

---
+++ 27_zadacha/Program.cs
@@ -0,0 +1,12 @@
+﻿// SeЗадача 27: Напишите программу, которая принимает 
+//на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Clear();
+Console.Write("Введите число a: ");
+string a = String(Console.ReadLine());
+int size = a.Length;
+int sum = 0;
+for (int i = 0; i < size; i++) sum += i;
+return sum;
